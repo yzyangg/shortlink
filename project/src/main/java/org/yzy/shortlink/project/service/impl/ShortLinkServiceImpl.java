bb0b9a610199aa3d43d16331e27d97f20288a011
mlxx <Crimsonteps@outlook.com>
2023-11-28 00:27:56 +0800

    完成短链接创建;

--- project/src/main/java/org/yzy/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/yzy/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -4,13 +4,18 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import org.yzy.shortlink.project.common.convention.exception.ServiceException;
 import org.yzy.shortlink.project.dao.entity.ShortLinkDO;
 import org.yzy.shortlink.project.dao.mapper.ShortLinkMapper;
 import org.yzy.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import org.yzy.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import org.yzy.shortlink.project.service.ShortLinkService;
 import org.yzy.shortlink.project.util.HashUtil;
+
+import java.util.Objects;
 
 
 /**
@@ -21,19 +26,52 @@ import org.yzy.shortlink.project.util.HashUtil;
 @RequiredArgsConstructor
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
 
+    private final RBloomFilter<String> shortURICreateCachePenetrationBloomFilter;
+
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO shortLinkCreateReqDTO) {
         String shortURI = generateSuffix(shortLinkCreateReqDTO);
         ShortLinkDO shortLinkDO = BeanUtil.copyProperties(shortLinkCreateReqDTO, ShortLinkDO.class);
+        String fullShortUrl = shortLinkCreateReqDTO.getDomain() + "/" + shortURI;
+
         shortLinkDO.setShortUri(shortURI);
         shortLinkDO.setEnableStatus(0);
-        shortLinkDO.setFullShortUrl(shortLinkCreateReqDTO.getDomain() + "/" + shortURI);
-        baseMapper.insert(shortLinkDO);
+        shortLinkDO.setFullShortUrl(fullShortUrl);
+
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (DuplicateKeyException e) {
+            // 缓存中不存在，数据库中存在
+            log.info("短链接已存在 {}", fullShortUrl);
+            throw new ServiceException("短链接已存在");
+        }
+        shortURICreateCachePenetrationBloomFilter.add(fullShortUrl);
 
         return BeanUtil.copyProperties(shortLinkDO, ShortLinkCreateRespDTO.class);
     }
 
-    public static String generateSuffix(ShortLinkCreateReqDTO shortLinkCreateReqDTO) {
-        return HashUtil.generateShortLink(shortLinkCreateReqDTO.getOriginUrl());
+    /**
+     * 生成短链接后缀
+     *
+     * @param shortLinkCreateReqDTO 短链接创建请求参数
+     * @return 短链接后缀
+     */
+    public String generateSuffix(ShortLinkCreateReqDTO shortLinkCreateReqDTO) {
+        String shortURI = "";
+        String originUrl = shortLinkCreateReqDTO.getOriginUrl();
+        String domain = shortLinkCreateReqDTO.getDomain();
+        int maxFrequencyCount = 1000;
+        int currentFrequencyCount = 0;
+        while (true) {
+            if (currentFrequencyCount >= maxFrequencyCount) {
+                throw new ServiceException("生成短链接失败");
+            }
+            shortURI = HashUtil.generateShortLink(originUrl);
+            if (!shortURICreateCachePenetrationBloomFilter.contains(domain + "/" + shortURI)) {
+                break;
+            }
+            currentFrequencyCount++;
+        }
+        return HashUtil.generateShortLink(Objects.requireNonNull(shortURI));
     }
 }
---
+++ project/src/main/java/org/yzy/shortlink/project/web/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package org.yzy.shortlink.project.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.yzy.shortlink.project.common.convention.errorcode.BaseErrorCode;
+import org.yzy.shortlink.project.common.convention.exception.AbstractException;
+import org.yzy.shortlink.project.common.convention.result.Result;
+import org.yzy.shortlink.project.common.convention.result.Results;
+
+/**
+ * @author yzy
+ * @version 1.0
+ * @description TODO
+ * @date 2023/11/19 22:06
+ */
+@Component
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Result<Void>> handleValidationExceptions(
+            HttpServletRequest request, MethodArgumentNotValidException ex) {
+        String exceptionStr = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .findFirst()
+                .orElse("");
+        logError(request, ex, exceptionStr);
+        return ResponseEntity.badRequest().body(Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr));
+    }
+
+    @ExceptionHandler(AbstractException.class)
+    public ResponseEntity<Result<Void>> handleAbstractException(
+            HttpServletRequest request, AbstractException ex) {
+        logError(request, ex, ex.toString());
+        return ResponseEntity.badRequest().body(Results.failure(ex));
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<Result<Void>> handleDefaultError(
+            HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {}", request.getMethod(), getUrl(request), throwable);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Results.failure());
+    }
+
+    private void logError(HttpServletRequest request, Throwable ex, String message) {
+        String url = getUrl(request);
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), url, message, ex.getCause());
+        } else {
+            log.error("[{}] {} [ex] {}", request.getMethod(), url, message);
+        }
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
